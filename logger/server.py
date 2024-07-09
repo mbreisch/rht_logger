f9bd2042a67ba899fb2b20c4de2ee9b8a7fa2f4e
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-07-09 15:50:21 +0200

    modified:   logger/server.py

--- logger/server.py
+++ logger/server.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from threading import Thread
 import requests
+from time import sleep
 
 import relay
 import rht
-import client
 
 app = Flask(__name__)
 CORS(app)
@@ -29,6 +29,26 @@ def get_fan_status():
 def get_rht_status():
     return jsonify(rht.status)
 
+def run_client():
+    while True:
+        relay.fan_control_data = get_fan_control_data()
+        sleep(1)
+
+@app.route('/get_fan_control_data', methods=['GET'])
+def get_fan_control_data():
+    url = 'http://raspberrypisipm.am14.uni-tuebingen.de:5000/ambient/fan_control'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            relay.fan_control_data = response.json()
+            return response.json()
+        else:
+            print(f'Failed to fetch data. Status code: {response.status_code}')
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f'Request failed: {str(e)}')
+        return None
+
 def run_flask():
     app.run(host='0.0.0.0', port=5000)
     
@@ -37,7 +57,7 @@ if __name__ == "__main__":
         flask_thread.daemon = True
         flask_thread.start()
         
-        client_thread = Thread(target=client.run_client)
+        client_thread = Thread(target=run_client)
         client_thread.daemon = True
         client_thread.start()
         
